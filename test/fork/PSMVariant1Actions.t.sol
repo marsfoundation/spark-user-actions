4e03af4b51908747ae80e97c5bcf174ff9bf4d7d
Sam MacPherson <xylom44@gmail.com>
2024-07-31 09:21:45 -0400

    deploy the lite psm instance

--- test/fork/PSMVariant1Actions.t.sol
+++ test/fork/PSMVariant1Actions.t.sol
@@ -6,8 +6,6 @@ import "forge-std/Test.sol";
 import { IERC20 }   from "forge-std/interfaces/IERC20.sol";
 import { IERC4626 } from "forge-std/interfaces/IERC4626.sol";
 
-import { VmSafe } from "forge-std/Vm.sol";
-
 import { PSMVariant1Actions } from "src/PSMVariant1Actions.sol";
 
 interface PotLike {
